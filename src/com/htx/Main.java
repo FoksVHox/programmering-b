c0dc8ac16ebee92dfbcd2804619d1779783e444d
Jimmi Hansen <jimmi@xevotech.com>
2021-08-25 14:41:00 +0200

    Jeg hader dig Niels for at du valgte java...

--- src/com/htx/Main.java
+++ src/com/htx/Main.java
@@ -3,6 +3,6 @@ package com.htx;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("fuck java");
+        System.out.println("java");
     }
 }
---
+++ src/com/htx/MatematiskeFunktioner.java
@@ -0,0 +1,17 @@
+package com.htx;
+
+public class MatematiskeFunktioner {
+    public static void main(String[] arg) {
+        System.out.println("kvardratroden af 0 er "+Math.sqrt(0));
+        System.out.println("kvardratroden af 1 er "+Math.sqrt(1));
+        System.out.println("kvardratroden af 2 er "+Math.sqrt(2));
+        System.out.println("kvardratroden af 3 er "+Math.sqrt(3));
+        System.out.println("kvardratroden af 4 er "+Math.sqrt(4));
+        System.out.println("kvardratroden af 5 er "+Math.sqrt(5));
+        System.out.println("kvardratroden af 6 er "+Math.sqrt(6));
+        System.out.println("kvardratroden af 7 er "+Math.sqrt(7));
+        System.out.println("kvardratroden af 8 er "+Math.sqrt(8));
+        System.out.println("kvardratroden af 9 er "+Math.sqrt(9));
+        System.out.println("kvardratroden af 10 er "+Math.sqrt(10));
+    }
+}
